06254ea1eca717b8c06992fa795e10ea41d1efcf
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-12-08 08:39:15 +0530

    Saving local changes before merging route branch

--- backend/DTO/LoginRequestDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Backend.DTO;
-public class LoginRequestDto
-{
-    public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
-    public string Role { get; set; } = null!;
-}
