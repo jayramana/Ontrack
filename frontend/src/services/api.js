a6300c8948676a5f9be2bffc350a7cc369071a3a
Ayyappan-M-P <mpayyappan2004@gmail.com>
2025-12-23 22:50:00 +0530

    full working after render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
+export const API_BASE_URL = `https://ontrack-t99t.onrender.com/api`;
 
 
 // Create axios instance
